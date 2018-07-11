c924f8c3fb2dbe051d5f5872748462b46b64f258
yangjj <yangjinjin@qianbao.com>
2018-07-11 11:12:16 +0800

    前端下载文件-目前只兼容火狐和谷歌

---
+++ js/download.js
@@ -0,0 +1,8 @@
+// 直接下载，用户体验好
+function download2() {
+    var $form = $('<form method="GET"></form>');
+    $form.attr('action', '/download/papers/1');
+    $form.appendTo($('body'));
+    $form.submit();
+    console.log('--------------');
+}
