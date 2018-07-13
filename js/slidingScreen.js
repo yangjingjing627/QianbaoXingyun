4e979bc6b8b1b5c6d89aa6788a2556c66a696011
yangjj <yangjinjin@qianbao.com>
2018-07-13 16:43:32 +0800

    page2-第三页的动画处理

--- js/slidingScreen.js
+++ js/slidingScreen.js
@@ -1,12 +1,23 @@
 $(function() {
   var nowpage=0;
   var lock=true;
+  var $page2Main = $('#page2Main');
+
   function donghua(){
     $('.containter').animate({'top': -100*nowpage+'%'}, 500);
     $('.page').eq(nowpage).addClass('current').siblings().removeClass('current');
     $('.dian ul li').eq(nowpage).addClass('da').siblings().removeClass('da');
     $('.nav ul li').eq(nowpage).addClass('bg').siblings().removeClass('bg');
+    if (nowpage === 2) {
+      console.log('这是第三页');
+      $page2Main.addClass('page2-animate')
+    } else {
+      setTimeout(removeClass, 100);
+    }
   }
+  function removeClass(){
+		$page2Main.removeClass('page2-animate');
+	}
   $(document).mousewheel(function (event,delta) {
     if(lock){
       nowpage=nowpage-delta;
