dbf8e296e908a3cc9ec1b608245242b1f5c0d20b
yangjj <yangjinjin@qianbao.com>
2018-07-16 10:41:23 +0800

    优化界面

--- js/slidingScreen.js
+++ js/slidingScreen.js
@@ -18,6 +18,7 @@ $(function() {
 		$page2Main.removeClass('page2-animate');
 	}
   $(document).mousewheel(function (event,delta) {
+    console.log('-----delta = ' + delta);
     if(lock){
       nowpage=nowpage-delta;
       if(nowpage<0){nowpage=0;}
